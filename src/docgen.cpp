edfcb75a11bfdf8d53dadebf6744bf1fb8764b63
kentjhall <kentjhall3@hotmail.com>
2019-12-28 22:58:16 -0500

    Kent.hall/basic structure (#3)
    * initial version of docgen_args.hpp, has function/variables to parse argv for options and sets global variables accordingly; also includes cleanup function for freeing set heap-allocated pointers
    
    * renamed docgen_args.hpp to docgen_options.hpp and updated for setting global status on failure
    
    * created docgen.cpp for holding main function; docgen.hpp for holding global json parsed (megaJSON); docgen_parser.hpp and docgen_gen.hpp for holding functions relating to parsing code and generating docs, respectively
    
    * renamed/moved around some things in docgen_status.hpp; add more specific status codes for issues with file I/O
    
    * updated existing points of failure with file I/O to reflect new status codes; fixed handling of flags w/out an argument in docgen_options.hpp
    
    * I decided to conquer my fears and delve into the C++ world of exceptions; after reading more about it, it does seem that this will be preferable to the global status code system for usability (although I did read some sketch stuff about "stack unwinding" and some general uncertainty with how it all works, so thats a bit sketch), and this system also takes into account how C++ STL stuff and operators might fail (for example, the new operator will throw a bad_alloc exception on heap allocation failure), so I went ahead and overhauled my previous C-like status code system in favor of C++ exceptions
    
    * changed 'throw()' keyword in function declaration to 'noexcept', as apparently this standard was changed in C++11
    
    * went ahead and made all std::exception descendents constructors be 'noexcept', as this seems like it would make sense to do
    
    * Reorganized things such that headers are no longer used solely for the sake of modularization; basically everything is in docgen.cpp now
    
    * added exception for control flow and thrown at a particular point in docgen.cpp which shouldn't be reachable
    
    * changed globals 'parsed_src' and 'parsed_dst' to use shared_ptr, negating the need for a cleanup function; a bit of general requested reorganization
    
    * moved around the directory structure a bit for 'src' directory
    
    * added example_gen.js for example of what page-builder NodeJS scripts might look like
    
    * rearranged directory structure a bit again to put nodejs_scripts back at project_root; surface-level changes to docgen.cpp; removed the example_gen.js script from repo

---
+++ src/docgen.cpp
@@ -0,0 +1,138 @@
+#include <getopt.h>
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include "exceptions.hpp"
+
+/* Options */
+static const char * const *files_src_paths;
+static std::shared_ptr<std::istream> parsed_src(nullptr);
+static std::shared_ptr<std::ostream> parsed_dst(nullptr);
+static const char *docs_dst_path = ".";
+
+/*
+ * Iterates over flags from passed argv to set global options accordingly
+ */
+static inline void set_options(int argc, char **argv)
+{
+	int c;
+	while ((c = getopt(argc, argv, ":i:o:d:")) != -1) {
+		switch (c) {
+			case 'i':
+				if (strcmp(optarg, "-") == 0) {
+					parsed_src = std::shared_ptr<std::istream>(&std::cin, [](std::istream *p){});
+				}
+				else {
+					parsed_src = std::make_shared<std::ifstream>(optarg);
+					if (parsed_src->fail()) {
+						throw docgen::system_error("failed to open istream");
+					}
+				}
+				break;
+			case 'o':
+				if (strcmp(optarg, "-") == 0) {
+					parsed_dst = std::shared_ptr<std::ostream>(&std::cout, [](std::ostream *p){});
+				}
+				else {
+					parsed_dst = std::make_shared<std::ofstream>(optarg);
+					if (parsed_dst->fail()) {
+						throw docgen::system_error("failed to open ostream");
+					}
+				}
+				break;
+			case 'd':
+			{
+				size_t arg_len = strlen(optarg);
+				if (optarg[arg_len-1] == '/') {
+					optarg[arg_len-1] = 0;
+				}
+				docs_dst_path = optarg;
+				break;
+			}
+			case ':':
+				fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+				throw docgen::bad_flags();
+				break;
+			case '?':
+				if (isprint(optopt)) {
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				}
+				else {
+					fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+				}
+				throw docgen::bad_flags();
+				break;
+			default:
+				fprintf(stderr, "Encountered an unknown error parsing options.\n");
+				throw docgen::bad_flags();
+		}
+	}
+	files_src_paths = argv + optind;
+}
+
+/* Parsing data JSON */
+static nlohmann::json parsed;
+
+/*
+ * Populates global JSON with parsing data as per global options
+ */
+static inline void to_parsed()
+{
+	if (parsed_src) {
+		*parsed_src >> parsed;	
+		if (parsed_src->fail()) {
+			throw docgen::system_error("failed to read from istream");
+		}
+		return;
+	}
+
+	if (!files_src_paths) {
+		throw docgen::control_flow_error("global var 'files_src_paths' not set for routine 'to_parsed'");
+	}
+
+	// TODO parse files from global NULL-terminated list files_src_paths and write information to json parsed
+}
+
+/*
+ * Handles parsing data from global JSON as per global options
+ */
+static inline void from_parsed()
+{
+	if (parsed_dst) {
+		*parsed_dst << parsed;
+		if (parsed_dst->fail()) {
+			throw docgen::system_error("failed to write to ostream");
+		}
+		return;
+	}
+
+	// TODO generate docs (writing files as necessary) to global path docs_dst_path from json parsed
+}
+
+int main(int argc, char **argv)
+{
+	static int status = 0;
+
+	try {
+		set_options(argc, argv);
+
+		to_parsed();
+
+		from_parsed();
+	}
+	catch (const docgen::exception& de) {
+		std::cerr << de.what() << '\n';
+		status = 1;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Docgen encountered an error: " << e.what() << '\n';
+		status = 1;
+	}
+
+	return status;	
+}
---
+++ src/exceptions.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstdio>
+#include <cerrno>
+#include <string>
+#include <exception>
+
+namespace docgen
+{
+	class exception : public std::exception
+	{
+		public:
+			explicit exception(const std::string& msg) noexcept
+				: msg_(prefix_ + msg)
+			{}
+
+			virtual const char *what() const noexcept
+			{
+				return msg_.c_str();
+			}
+
+		private:
+			static constexpr const char *prefix_ = "Docgen encountered a problem: ";
+			std::string msg_;
+	};
+
+	class system_error : public exception
+	{
+		public:
+			explicit system_error(const std::string& msg) noexcept
+				: exception(msg + ": " + strerror(errno))
+			{}
+	};
+
+	class control_flow_error : public exception
+	{
+		public:
+			explicit control_flow_error(const std::string& msg) noexcept
+				: exception("control flow error: " + msg)
+			{}
+	};
+
+	class bad_flags : public exception
+	{
+		public:
+			bad_flags() noexcept
+				: exception("bad flags")
+			{}
+	};
+}
