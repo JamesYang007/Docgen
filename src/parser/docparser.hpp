f3ef494e1a09eff755e25de6566db46c25652b30
James Yang <jamesyang916@gmail.com>
2019-12-29 17:37:20 -0500

    Add read state and parse function for every state

--- src/parser/docparser.hpp
+++ src/parser/docparser.hpp
@@ -14,6 +14,7 @@ public:
 
     // Tags indicating possible states of a DocParser object.
     enum class State {
+        read,
         slash,
         single_line,
         block
@@ -21,10 +22,26 @@ public:
 
 private:
 
-    
+    // Parse function depending on a particular state.
+    template <State state>
+    const char *parse(const char *chunk, nlohmann::json& json);
 
     State state;
 
 };
+
+template <>
+const char *DocParser::parse<DocParser::State::read>(
+        const char *chunk, nlohmann::json& json)
+{
+    // TODO: implement parsing for read state
+}
+
+template <>
+const char *DocParser::parse<DocParser::State::slash>(
+        const char *chunk, nlohmann::json& json)
+{
+    // TODO: implement parsing for slash state
+}
 
 } // namespace docgen
