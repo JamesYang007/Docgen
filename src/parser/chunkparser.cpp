94df1b1faf1aad6106618b8df29bf646c19563f6
James Yang <jamesyang916@gmail.com>
2019-12-30 10:29:55 -0500

    Change < to != in parse public function

--- src/parser/chunkparser.cpp
+++ src/parser/chunkparser.cpp
@@ -6,7 +6,7 @@ void ChunkParser::parse(const char *begin,
                         const char *end, 
                         nlohmann::json& json)
 {
-    while (begin < end) {
+    while (begin != end) {
         // begin will get updated inside the parser function
         // the return value is the next state 
         state_ = parser_[state_](begin, end, json);            
