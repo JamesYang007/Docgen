1eeba62d2b1a86065052b4b2655dbfa445150d14
James Yang <jamesyang916@gmail.com>
2019-12-30 19:59:55 -0500

    James.yang/parser init (#4)
    * Ignore build directory
    
    * Move exceptions into its own subdirectory.
    
    * Add parser structure
    
    * Add read state and parse function for every state
    
    * Define public parse member function
    
    * Add break in switch-case statements
    
    * Add array of function pointers indexed by state enum values
    
    * Restructure parser with separated routines and array of routines
    
    * Update comment in details routines
    
    * Move free functions to core
    
    * Change < to != in parse public function
    
    * Remove slash state
    
    * Add json as private member of ChunkParser and change member function prototype accordingly
    
    * Rename json to parser_
    
    * Add public getter function for parsed_

---
+++ src/parser/core/routine.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <nlohmann/json.hpp>
+
+namespace docgen {
+namespace core {
+
+// These routines will parse the chunk of text starting at begin
+// until it reaches end and update the json object.
+// When a state changes or the chunk has been fully read, the routine will finish.
+inline void read_routine(const char *&begin, const char *end, nlohmann::json& json)
+{
+    // TODO: implement parsing 
+}
+
+inline void single_line_routine(const char *&begin, const char *end, nlohmann::json& json)
+{
+    // TODO: implement parsing 
+}
+
+inline void block_routine(const char *&begin, const char *end, nlohmann::json& json)
+{
+    // TODO: implement parsing 
+}
+
+} // namespace core
+} // namespace docgen
