9da546dd3965260a856bcc6d1daed6811574b4f6
James Yang <jamesyang916@gmail.com>
2019-12-31 20:54:40 -0500

    Add slash unit test

--- test/parse/core/routine_unittest.cpp
+++ test/parse/core/routine_unittest.cpp
@@ -12,7 +12,15 @@ protected:
     using cache_t = Cache<10>;
     using routine_array_t = routine_array_t<cache_t>;
     static constexpr routine_array_t routines = make_routines<cache_t>();
+    cache_t cache;
+    std::string doc = 
+        "#include <some_dir/some_header.hpp>\n"
+        "\n"
+        "  //   some comment"
+        ;
 };
+
+// make_routines TEST
     
 TEST_F(routine_fixture, routine_array_constexprness)
 {
@@ -26,6 +34,19 @@ TEST_F(routine_fixture, routine_array_ordering)
     EXPECT_EQ(routines[0], &routine<static_cast<Routine>(0)>::template run<cache_t>);
     EXPECT_EQ(routines[1], &routine<static_cast<Routine>(1)>::template run<cache_t>);
     EXPECT_EQ(routines[2], &routine<static_cast<Routine>(2)>::template run<cache_t>);
+}
+
+// process_slash TEST
+TEST_F(routine_fixture, slash_end_batch)
+{
+    using namespace details;
+    cache.symbol.clear();
+    cache.symbol.push_back('/');
+    const char* begin = doc.c_str() + doc.length();
+    const char* end = begin;
+    Routine actual = process_slash(cache, begin, end); 
+    Routine expected = Routine::READ;
+    EXPECT_EQ(actual, expected);
 }
 
 } // namespace core
