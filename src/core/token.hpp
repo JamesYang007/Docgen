187f62d7107f97d5546a32207b32ebee4b9216c7
James Yang <jamesyang916@gmail.com>
2020-01-10 10:28:15 -0500

    Add inline c_str member fn for token

--- src/core/token.hpp
+++ src/core/token.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include "symbol.hpp"
 
 namespace docgen {
 namespace core {
@@ -13,19 +14,24 @@ struct Token
         : name(name)
     {}
 
-    Token(symbol_t name, const std::string& content)
-        : name(name)
-        , content(content)
-    {}
-
     Token(symbol_t name, std::string&& content)
         : name(name)
         , content(std::move(content))
     {}
 
+    // left undefined for SymbolType != Symbol
+    const char* c_str() const;
+
     symbol_t name;
     std::string content;
 };
+
+template <>
+inline const char* Token<Symbol>::c_str() const
+{
+    return (symbol_map.find(name) != symbol_map.end()) ?
+        symbol_map.at(name).c_str() : content.c_str();
+}
 
 } // namespace core
 } // namespace docgen
