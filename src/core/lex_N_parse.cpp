53ccf7fed3faa1c67e590fa6d32474c12ab9e998
Kent <kentjhall3@hotmail.com>
2020-01-12 14:33:34 -0500

    spacing issue lex_N_parse.cpp

--- src/core/lex_N_parse.cpp
+++ src/core/lex_N_parse.cpp
@@ -11,7 +11,6 @@ void parse_file(const char *path, nlohmann::json *parsed)
 
 	core::Parser parser;
 	parser.process(lexer.get_tokens());
-	
 
 	parsed->push_back(std::move(parser.parsed()));
 	parsed->back()["name"] = path;
