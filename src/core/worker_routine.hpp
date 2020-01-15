f459ccb918222dc63ee6d2ef184e4deaf6c6ae29
Kent <kentjhall3@hotmail.com>
2020-01-14 22:28:17 -0500

    made WorkerRoutine a template class with WorkerType as template parameter, from which it discerns token_t (TokenType) and dest_t (DestType)

--- src/core/worker_routine.hpp
+++ src/core/worker_routine.hpp
@@ -1,13 +1,18 @@
 #pragma once
 
-#include "parse_feeder.hpp"
-
 namespace docgen {
 namespace core {
 
+/*
+ * WorkerType must have type aliases for token_t and dest_t
+ */
+template <class WorkerType>
 struct WorkerRoutine
 {
-	using routine_t = void (*)(const ParseFeeder::token_t&, ParseFeeder&);
+	using worker_t = WorkerType;
+	using tok_t = typename WorkerType::token_t;
+	using dest_t = typename WorkerType::dest_t;
+	using routine_t = void (*)(WorkerType *, const token_t&, dest_t&);
 };
 
 } // namespace core
