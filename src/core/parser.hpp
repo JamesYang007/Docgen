8365a8e1030accadfe15bef841da7b1068525837
Kent <kentjhall3@hotmail.com>
2020-01-14 22:45:10 -0500

    updated parser.hpp to work around new structure (making use of docgen_worker.hpp's parser_internal namespace); added FuncWorker() to be used

--- src/core/parser.hpp
+++ src/core/parser.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <vector>
 #include <initializer_list>
 #include <nlohmann/json.hpp>
-#include "parse_feeder.hpp"
+#include "json_writer.hpp"
 #include "parse_worker.hpp"
-#include "comment_worker.hpp"
-#include "ignore_worker.hpp"
+#include "parser_internal/docgen_worker.hpp"
+#include "parser_internal/comment_worker.hpp"
+#include "parser_internal/func_worker.hpp"
+#include "parser_internal/ignore_worker.hpp"
+#include "parser_internal/tag_worker.hpp"
 
 namespace docgen {
 namespace core {
@@ -13,31 +17,41 @@ namespace core {
 class Parser
 {
 	public:
+		using CommentWorker = parser_internal::CommentWorker;
+		using FuncWorker = parser_internal::FuncWorker;
+		using IgnoreWorker = parser_internal::IgnoreWorker;
+		using TagWorker = parser_internal::TagWorker;
+
 		Parser()
 			: worker_ {
-				CommentWorker(Symbol::BEGIN_LINE_COMMENT, Symbol::NEWLINE),
+				CommentWorker(Symbol::BEGIN_LINE_COMMENT, Symbol::NEWLINE, { TagWorker() }),
 				CommentWorker(Symbol::BEGIN_BLOCK_COMMENT, Symbol::END_BLOCK_COMMENT, {
-					IgnoreWorker(Symbol::NEWLINE, Symbol::STAR)
-				})
+					TagWorker(),
+					IgnoreWorker(Symbol::NEWLINE, Symbol::STAR).until_not()
+				}),
+				FuncWorker()
 			}
 		{}
 
-		void process(const std::vector<token_t>& tokens);
+		using token_t = parser_internal::token_class_t;
+		using token_arr_t = std::vector<token_t>;
 
-		const nlohmann::json& parsed() { return feeder_.parsed(); }
+		void process(const token_arr_t& tokens);
 
-	private:
-		ParseWorker worker_;
-		ParseFeeder feeder_;
+		const nlohmann::json& parsed() { return writer_.stored(); }
+
+	private:	
+		parser_internal::worker_class_t worker_;
+		parser_internal::writer_class_t writer_;
 };
 
-inline void Parser::process(const std::vector<token_t>& tokens)
+inline void Parser::process(const token_arr_t& tokens)
 {
-	feeder_.reset();
+	writer_.reset();
 
 	for (const token_t& token : tokens) {
-		worker_.proc(token, feeder_);
-		feeder_.feed(token);
+		worker_.proc(token, writer_);
+		writer_.feed(token);
 	}
 }
 
