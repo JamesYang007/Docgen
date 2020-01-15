a84708ee02422899ac31f7994b1f1d63ed63afc6
Kent <kentjhall3@hotmail.com>
2020-01-15 08:12:48 -0500

    parser.hpp: added type alias symbol_t for parser_internal::symbol_internal_t

--- src/core/parser.hpp
+++ src/core/parser.hpp
@@ -21,13 +21,14 @@ class Parser
 		using FuncWorker = parser_internal::FuncWorker;
 		using IgnoreWorker = parser_internal::IgnoreWorker;
 		using TagWorker = parser_internal::TagWorker;
+		using symbol_t = parser_internal::symbol_internal_t;
 
 		Parser()
 			: worker_ {
-				CommentWorker(symbol_internal_t::BEGIN_LINE_COMMENT, symbol_internal_t::NEWLINE, { TagWorker() }),
-				CommentWorker(symbol_internal_t::BEGIN_BLOCK_COMMENT, symbol_internal_t::END_BLOCK_COMMENT, {
+				CommentWorker(symbol_t::BEGIN_LINE_COMMENT, symbol_t::NEWLINE, { TagWorker() }),
+				CommentWorker(symbol_t::BEGIN_BLOCK_COMMENT, symbol_t::END_BLOCK_COMMENT, {
 					TagWorker(),
-					IgnoreWorker(symbol_internal_t::NEWLINE, symbol_internal_t::STAR).until_not()
+					IgnoreWorker(symbol_t::NEWLINE, symbol_t::STAR).until_not()
 				}),
 				FuncWorker()
 			}
