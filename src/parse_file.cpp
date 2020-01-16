29ea520763f66ba7640e49360f8a88afee1df86b
Kent <kentjhall3@hotmail.com>
2020-01-16 10:02:40 -0500

    added file IO to parse_file() (s.t. FILE * is passed to lexer)

--- src/parse_file.cpp
+++ src/parse_file.cpp
@@ -1,19 +1,29 @@
+#include <cstdio>
+#include <string>
+#include "exceptions/exceptions.hpp"
+#include "core/lexer.hpp"
+#include "core/parser.hpp"
 #include "parse_file.hpp"
 
 namespace docgen {
 
-using namespace core;
-
 void parse_file(const char *path, nlohmann::json& parsed)
 {
-	Lexer lexer(path);
+	FILE *file = fopen(path, "r");
+	if (file == nullptr) {
+		throw exceptions::system_error("failed to open file at path '" + std::string(path) + '\'');
+	}
+
+	core::Lexer lexer(file);
 	lexer.process();
 
-	Parser parser;
+	core::Parser parser;
 	parser.process(lexer.get_tokens());
 
 	parsed.push_back(std::move(parser.parsed()));
 	parsed.back()["name"] = path;
+
+	fclose(file);
 }
 
 } // namespace docgen
