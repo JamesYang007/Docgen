f9925226d522224c6bfe1161c953c867a7afa628
Kent <kentjhall3@hotmail.com>
2020-01-15 20:12:40 -0500

    updated workers to hold static routines in private struct rather than inheriting struct

--- src/core/parser_internal/comment_worker.hpp
+++ src/core/parser_internal/comment_worker.hpp
@@ -7,27 +7,7 @@ namespace docgen {
 namespace core {
 namespace parser_internal {
 
-struct CommentWorker_routines : private routine_details_t
-{
-	private:
-		static constexpr const char *DEFAULT_KEY = "desc";
-
-	protected:
-		using token_t = typename routine_details_t::token_t;
-
-		static constexpr const routine_t on_open_ = [](worker_t *, const token_t& t, dest_t& writer) {
-			if (!writer.key_set()) {
-				writer.set_key(DEFAULT_KEY);
-			}
-			writer.start_writing();
-			writer.skip_write();
-		};
-		static constexpr const routine_t on_close_ = [](worker_t *, const token_t&, dest_t& writer) {
-			writer.stop_writing();
-		};
-};
-
-class CommentWorker : public worker_t, private CommentWorker_routines
+class CommentWorker : public worker_t
 {
 	public:
 		using worker_t = typename parser_internal::worker_t;
@@ -35,10 +15,29 @@ class CommentWorker : public worker_t, private CommentWorker_routines
 
 		CommentWorker(token_t open, token_t close, worker_arr_init_t workers={})
 			: worker_t {
-				TokenHandler(open, on_open_),
-				TokenHandler(close, on_close_, workers)
+				TokenHandler(open, Routines::on_open_),
+				TokenHandler(close, Routines::on_close_, workers)
 			}
 		{}
+
+	private:
+		static constexpr const char *DEFAULT_KEY = "desc";
+
+		struct Routines : private routine_details_t
+		{
+			using token_t = typename routine_details_t::token_t;
+
+			static constexpr const routine_t on_open_ = [](worker_t *, const token_t& t, dest_t& writer) {
+				if (!writer.key_set()) {
+					writer.set_key(DEFAULT_KEY);
+				}
+				writer.start_writing();
+				writer.skip_write();
+			};
+			static constexpr const routine_t on_close_ = [](worker_t *, const token_t&, dest_t& writer) {
+				writer.stop_writing();
+			};		
+		};
 };
 
 } // namespace parser_internal
