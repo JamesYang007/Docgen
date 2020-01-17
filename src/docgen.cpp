8ba60c244d2d79c5968e6204eafac9e414bb5581
Kent <kentjhall3@hotmail.com>
2020-01-17 00:50:00 -0500

    added directory traversal; added options for setting logging and error streams

--- src/docgen.cpp
+++ src/docgen.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <memory>
+#include <filesystem>
 #include <nlohmann/json.hpp>
 #include "exceptions/exceptions.hpp"
 #include "parse_file.hpp"
@@ -16,6 +16,8 @@ using namespace docgen;
 static const char * const *files_src_paths;
 static std::shared_ptr<std::istream> parsed_src(nullptr);
 static std::shared_ptr<std::ostream> parsed_dst(nullptr);
+static std::shared_ptr<std::ostream> logger(&std::cerr, [](std::ostream *){});
+static std::shared_ptr<std::ostream> err(&std::cerr, [](std::ostream *){});
 static const char *docs_dst_path = ".";
 
 /*
@@ -24,28 +26,40 @@ static const char *docs_dst_path = ".";
 static inline void set_options(int argc, char **argv)
 {
 	int c;
-	while ((c = getopt(argc, argv, ":i:o:d:")) != -1) {
+	while ((c = getopt(argc, argv, ":i:o:l:e:d:")) != -1) {
 		switch (c) {
 			case 'i':
 				if (strcmp(optarg, "-") == 0) {
-					parsed_src = std::shared_ptr<std::istream>(&std::cin, [](std::istream *p){});
+					parsed_src = std::shared_ptr<std::istream>(&std::cin, [](std::istream *){});
 				}
 				else {
 					parsed_src = std::make_shared<std::ifstream>(optarg);
 					if (parsed_src->fail()) {
-						throw exceptions::system_error("failed to open istream");
+						throw exceptions::file_open_error(optarg);
 					}
 				}
 				break;
 			case 'o':
 				if (strcmp(optarg, "-") == 0) {
-					parsed_dst = std::shared_ptr<std::ostream>(&std::cout, [](std::ostream *p){});
+					parsed_dst = std::shared_ptr<std::ostream>(&std::cout, [](std::ostream *){});
 				}
 				else {
 					parsed_dst = std::make_shared<std::ofstream>(optarg);
 					if (parsed_dst->fail()) {
-						throw exceptions::system_error("failed to open ostream");
+						throw exceptions::system_error("failed to open output stream");
 					}
+				}
+				break;
+			case 'l':
+				logger = std::make_shared<std::ofstream>(optarg, std::ofstream::app);
+				if (logger->fail()) {
+					throw exceptions::system_error("failed to open logging stream");
+				}
+				break;
+			case 'e':
+				err = std::make_shared<std::ofstream>(optarg, std::ofstream::app);
+				if (err->fail()) {
+					throw exceptions::system_error("failed to open error stream");
 				}
 				break;
 			case 'd':
@@ -89,7 +103,7 @@ static inline void to_parsed()
 	if (parsed_src) {
 		*parsed_src >> parsed;	
 		if (parsed_src->fail()) {
-			throw exceptions::system_error("failed to read from istream");
+			throw exceptions::system_error("failed to read from input stream");
 		}
 		return;
 	}
@@ -98,9 +112,34 @@ static inline void to_parsed()
 		throw exceptions::control_flow_error("global var 'files_src_paths' not set for routine 'to_parsed'");
 	}
 
+	namespace fs = std::filesystem;
+
 	while (*files_src_paths) {
-		parse_file(*files_src_paths++, parsed);
-		// TODO handle directories
+		fs::file_status status = fs::status(*files_src_paths);
+
+		if (fs::is_regular_file(status)) {
+			*logger << "Processing \"" << *files_src_paths << '\"' << '\n';
+			parse_file(*files_src_paths, parsed);
+		}
+		else if (fs::is_directory(status)) {
+			for (const fs::directory_entry& entry : fs::recursive_directory_iterator(*files_src_paths)) {
+				if (entry.is_regular_file()) {
+					*logger << "Processing " << entry.path() << '\n';
+					parse_file(entry.path().c_str(), parsed);
+				}
+				else if (!entry.is_directory()) {
+					*logger << "Skipping " << entry.path() << " (not regular file)" << '\n';
+				}
+			}
+		}
+		else if (!fs::exists(status)) {
+			throw exceptions::file_exist_error(*files_src_paths);
+		}
+		else {
+			throw exceptions::bad_file(*files_src_paths);
+		}
+
+		++files_src_paths;
 	}
 }
 
@@ -109,10 +148,14 @@ static inline void to_parsed()
  */
 static inline void from_parsed()
 {
+	if (parsed.is_null()) {
+		throw exceptions::bad_source();
+	}
+
 	if (parsed_dst) {
 		*parsed_dst << parsed;
 		if (parsed_dst->fail()) {
-			throw exceptions::system_error("failed to write to ostream");
+			throw exceptions::system_error("failed to write to output stream");
 		}
 		return;
 	}
@@ -132,11 +175,11 @@ int main(int argc, char **argv)
 		from_parsed();
 	}
 	catch (const exceptions::exception& de) {
-		std::cerr << de.what() << '\n';
+		*err << de.what() << '\n';
 		status = 1;
 	}
 	catch (const std::exception& e) {
-		std::cerr << "Docgen encountered an error: " << e.what() << '\n';
+		*err << "Docgen encountered an error: " << e.what() << '\n';
 		status = 1;
 	}
 
