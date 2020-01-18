68b59ed3582739e1835066848124b3cdce20c288
James Yang <jamesyang916@gmail.com>
2020-01-18 15:08:01 -0500

    Modify configuration to fix at release points for libs

--- configure.sh
+++ configure.sh
@@ -1,14 +1,35 @@
-#!/bin/sh
+#!/bin/bash
+
+# directory where current shell script resides
+PROJECTDIR=$(dirname "$BASH_SOURCE")
+
+cd "$PROJECTDIR"
 
 # If setup.sh was called before
 if [ -d "libs/benchmark/googletest" ]; then
     rm -rf libs/benchmark
 fi
 
+# Initialize submodules if needed
+git submodule init
 # Update submodule if needed
 git submodule update --remote
-# Setup google benchmark and googletest
+
+# Setup googletest
 git clone https://github.com/google/googletest.git libs/benchmark/googletest
+# Set google test to specific release tag
+cd libs/benchmark/googletest
+git fetch --all --tags --prune
+git checkout tags/release-1.10.0 -b release-1.10.0
+cd -
+
+# Setup googlebenchmark
+cd libs/benchmark
+git fetch --all --tags --prune
+git checkout tags/v1.5.0 -b v1.5.0
+cd -
+
+# Build google benchmark
 cd libs/benchmark
 mkdir -p build && cd build
 
