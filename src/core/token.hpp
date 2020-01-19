dff712f4b8c9294271443d5edba6b4082fd0a71b
Kent <kentjhall3@hotmail.com>
2020-01-18 22:42:32 -0500

    added operator!= for Token

--- src/core/token.hpp
+++ src/core/token.hpp
@@ -37,6 +37,10 @@ inline bool operator==(const Token<Symbol>& t1, const Token<Symbol>& t2) {
 	return t1.name == t2.name;
 }
 
+inline bool operator!=(const Token<Symbol>& t1, const Token<Symbol>& t2) {
+	return !(t1 == t2);
+}
+
 } // namespace core
 } // namespace docgen
 
