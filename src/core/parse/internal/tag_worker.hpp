b3df36439a9008dbd23afe8897c8c4084f983774
Kent <kentjhall3@hotmail.com>
2020-01-20 12:31:09 -0500

    added support for specialized param tag behavior to TagWorker

--- src/core/parse/internal/tag_worker.hpp
+++ src/core/parse/internal/tag_worker.hpp
@@ -17,19 +17,40 @@ class TagWorker : public worker_t
 					symbol_t::TPARAM,
 					symbol_t::PARAM,
 					symbol_t::RETURN
-				}, Routines::on_tag_)
+				}, Routines::on_tag_),
+				TokenHandler(symbol_t::TEXT, nullptr)
 			}
 		{}
 
 	private:
+		static constexpr const char * const PARAMS_KEY = "params";
+		static constexpr const char * const PARAM_NAME_KEY = "name";
+		static constexpr const char * const PARAM_DESC_KEY = "desc";
+
 		struct Routines : private routine_details_t
 		{
 			using token_t = routine_details_t::token_t;
 
-			static constexpr const routine_t on_tag_  = [](worker_t *, const token_t& t, dest_t& writer) {
-				writer.set_key(t.c_str());
+			static constexpr const routine_t on_param_first_text_  = [](worker_t *, const token_t& token, dest_t& writer) {
+				writer.stow_for(PARAMS_KEY);
+				writer.set_key_active(PARAM_NAME_KEY);
+				writer.write(token.c_str());
+				writer.set_key_active(PARAM_DESC_KEY);
 				writer.start_writing();
 				writer.skip_write();
+			};
+			static constexpr const routine_t on_tag_  = [](worker_t *worker, const token_t& token, dest_t& writer) {
+				if (token == symbol_t::PARAM) {
+					worker->handler(1).on_match(on_param_first_text_);
+					if (writer.writing()) {
+						writer.skip_write();
+					}
+					return;
+				}
+				writer.set_key(token.c_str());
+				writer.start_writing();
+				writer.skip_write();
+				worker->restart();
 			};
 		};
 };
