89346880b690dd6115fa00f746ff95b113a4bce1
Kent <kentjhall3@hotmail.com>
2020-01-22 16:29:26 -0500

    moved parser.hpp worker initialization to new GeneralWorker

--- src/core/parse/parser.hpp
+++ src/core/parse/parser.hpp
@@ -3,10 +3,8 @@
 #include <initializer_list>
 #include <nlohmann/json.hpp>
 #include "core/parse/details.hpp"
-#include "core/parse/internal/comment_worker.hpp"
-#include "core/parse/internal/func_worker.hpp"
-#include "core/parse/internal/ignore_worker.hpp"
-#include "core/parse/internal/tag_worker.hpp"
+#include "core/parse/internal/general_worker.hpp"
+#include "core/parse/internal/class_worker.hpp"
 
 namespace docgen {
 namespace core {
@@ -15,37 +13,13 @@ namespace parse {
 class Parser
 {
 	public:
-		using CommentWorker = internal::CommentWorker;
-		using FuncWorker = internal::FuncWorker;
-		using IgnoreWorker = internal::IgnoreWorker;
-		using TagWorker = internal::TagWorker;
-
-		Parser()
-			: worker_ {
-				IgnoreWorker(symbol_t::HASHTAG, symbol_t::NEWLINE),
-				IgnoreWorker(symbol_t::BEGIN_NLINE_COMMENT, symbol_t::NEWLINE),
-				IgnoreWorker(symbol_t::BEGIN_NBLOCK_COMMENT, symbol_t::END_BLOCK_COMMENT).ignore_last(),
-				CommentWorker(symbol_t::BEGIN_SLINE_COMMENT, symbol_t::NEWLINE, {
-					TagWorker(),
-					IgnoreWorker(
-						{ symbol_t::BEGIN_SLINE_COMMENT, symbol_t::BEGIN_NLINE_COMMENT, symbol_t::FORWARD_SLASH },
-						symbol_t::END_OF_FILE
-					).clear_key().timeout(1).limit(1)
-				}),
-				CommentWorker(symbol_t::BEGIN_SBLOCK_COMMENT, symbol_t::END_BLOCK_COMMENT, {
-					TagWorker(),
-					IgnoreWorker(symbol_t::NEWLINE, { symbol_t::STAR, symbol_t::WHITESPACE, symbol_t::NEWLINE }).until_not()
-				}),
-				FuncWorker()
-			}
-		{}
-
 		void process(const token_t& token);
 
 		nlohmann::json& parsed() { return writer_.stored(); }
+		const nlohmann::json& parsed() const { return writer_.stored(); }
 
 	private:	
-		worker_t worker_;
+		worker_t worker_ = internal::GeneralWorker().inject_worker_at(0, internal::ClassWorker());
 		writer_t writer_;
 };
 
@@ -55,6 +29,6 @@ inline void Parser::process(const token_t& token)
 	writer_.feed(token.c_str());
 }
 
-}
+} // namespace parse
 } // namespace docgen
 } // namespace core
